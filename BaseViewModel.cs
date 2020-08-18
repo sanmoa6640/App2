e8657a299df3341cac65848cb26269147ce4c221
sanmoa6640 <59384822+sanmoa6640@users.noreply.github.com>
2020-08-18 13:04:46 -0500

    Add project files.

---
+++ BaseViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App2
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected void SetProperty<T>(ref T property, T value,
+        [CallerMemberName] string propertyName = "")
+        {
+            property = value;
+            OnPropertyChanged(propertyName);
+        }
+
+        protected void OnPropertyChanged2([CallerMemberName] string propertyName = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
---
+++ Recording.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Composition;
+using Windows.UI.Popups;
+
+namespace App2
+{
+    public class Recording : BaseViewModel
+    {
+        private string artistName;
+        public string ArtistName
+        {
+            get { return artistName; }
+            set 
+            {   SetProperty(ref artistName, value);
+                OnPropertyChanged2();
+            }
+        }
+
+        private string compositionName;
+        public string CompositionName
+        {
+            get { return compositionName; }
+            set 
+            {   SetProperty(ref compositionName, value);
+                OnPropertyChanged2();
+            }
+        }
+
+        private DateTime releaseDateTime;
+        public DateTime ReleaseDateTime 
+        { 
+            get { return releaseDateTime; } 
+            set { SetProperty(ref releaseDateTime, value); }
+        }
+
+        public Recording()
+        {
+            this.ArtistName = "Wolfgang Amadeus Mozart";
+            this.CompositionName = "Andante in C for Piano";
+            this.ReleaseDateTime = new DateTime(1761, 1, 1);
+        }
+        private String onlineSummary;
+        public string OneLineSummary
+        {
+            get
+            {
+                return $"{this.CompositionName} by {this.ArtistName}, released: "
+                    + this.ReleaseDateTime.ToString("d");
+            }
+            set
+            {
+                OnPropertyChanged2();
+            }
+            
+        }
+
+    }
+    public class RecordingViewModel : BaseViewModel
+    {
+        private Recording defaultRecording = new Recording();
+        public Recording DefaultRecording { get { return this.defaultRecording; } set { SetProperty(ref this.defaultRecording, value); OnPropertyChanged2(); } }
+        public RecordingViewModel()
+        {
+            
+        }
+        public async void MakeSomeAsync()
+        {
+            var message = "Santiago Morales ";
+
+            var messageDialog = new MessageDialog(message, "Person data:");
+
+            await messageDialog.ShowAsync();
+        }
+    }
+}
